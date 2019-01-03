6212646472621a6bee7f1553b31cb161468e1d59
mandercs3@gmail.comChris Mander <mandercs3@gmail.com>
2019-01-03 21:22:06 +0000

    Removing unsafe noexcept.

--- cpp/017.cpp
+++ cpp/017.cpp
@@ -14,7 +14,7 @@ using raw_nums = unordered_map<int, string>;
  *
  * Would be nice to account for ranges with an upper bound of over a thousand. Perhaps a story for another day?
  */
-raw_nums get_raw_num_map() noexcept
+raw_nums get_raw_num_map()
 {
   return raw_nums(
     {{1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}, {6, "six"}, {7, "seven"}, {8, "eight"},
